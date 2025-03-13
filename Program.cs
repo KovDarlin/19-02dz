0d723f0ef4faa21140dc1a5684001eba0727c5d8
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-03-13 23:21:20 +0200

    ex 2 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Football
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public Football(string name, string position)
+    {
+        Name = name;
+        Position = position;
+    }
+    public override string ToString()
+    {
+        return $"{Name} ({Position})";
+    }
+}
+
+public class Team : IEnumerable<Football>
+{
+    private readonly List<Football> players = new List<Football>();
+    public void Add(Football player) => players.Add(player);
+    public IEnumerator<Football> GetEnumerator() => players.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        var team = new Team
+        {
+            new("Lionel Messi", "Striker"),
+            new("Cristiano Ronaldo", "Striker"),
+            new("Manuel Neuer", "Goalkeeper")
+
+        };
+        Console.WriteLine("Football players:");
+        foreach (var player in team)
+            Console.WriteLine(player);
+
+    }
+}
